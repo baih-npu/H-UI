1e7053077a9f9243babb0805b28cde5461af9b6d
hyh3623290 <13999517333@163.com>
2021-04-24 18:31:10 +0800

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/Menu'
+import MenuItem from './components/Menu/MenuItem'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0} onSelect={(index) => {alert(index)}}>
+          <MenuItem index={0} disabled>cool link</MenuItem>
+          <MenuItem index={1}>cool link2</MenuItem>
+          <MenuItem index={2}>cool link3</MenuItem>
+        </Menu>
         <Button btnType={ButtonType.Default} onClick={() => { console.log('btn1') }}>
           Button1
         </Button>
